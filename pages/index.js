efd05f8a3bed446afd64e26522c9e1f8e1520490
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-06-02 20:37:34 +0200

    Finish Step of creating dynamic Pages

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,17 @@
 import Link from 'next/link';
 
 import Layout from '../components/Layout';
+import PostLink from '../components/PostLink';
 
 const Index = () => (
     <Layout>
-        <p>Hello Next.js</p>
+        <h1>My Blog</h1>
+
+        <ul>
+            <PostLink title="Hello Next.js"></PostLink>
+            <PostLink title="Learn Next.js is awensome"></PostLink>
+            <PostLink title="Deploy apps with Zeit"></PostLink>
+        </ul>
     </Layout>
 );
 
---
+++ pages/post.js
@@ -0,0 +1,11 @@
+import Layout from '../components/Layout';
+
+const Post = (props) => (
+    <Layout>
+        <h1>{props.url.query.title}</h1>
+
+        <p>This is the blog post content</p>
+    </Layout>
+);
+
+export default Post;
