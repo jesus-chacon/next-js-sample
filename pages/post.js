e24606b3a019361d7e8ec6cac1bc8e48670d6cd9
Jesús Cautivo Chacón <cautivo.chacon@gmail.com>
2018-06-03 15:05:33 +0200

    Add sample global styles for jsx

--- pages/post.js
+++ pages/post.js
@@ -1,5 +1,8 @@
+import Markdown from 'react-markdown';
+
 import Layout from '../components/Layout';
 
+/*
 const Post = (props) => (
     <Layout>
         <h1>{props.show.name}</h1>
@@ -7,6 +10,44 @@ const Post = (props) => (
         <p>{props.show.summary.replace(/<[/]?p>/g, '')}</p>
 
         <img src={props.show.image.medium} />
+    </Layout>
+);*/
+
+const Post = (props) => (
+    <Layout>
+        <h1>{props.show.name}</h1>
+
+        <div className="markdown">
+            <Markdown source={`
+                This is our blog post.
+                Yes. We can have a [link](/link).
+                And we can have a title as well.
+
+                ### This is a title
+
+                And here's the content.`} />
+        </div>
+
+        <style jsx global>{`
+            .markdown {
+            font-family: 'Arial';
+            }
+
+            .markdown a {
+            text-decoration: none;
+            color: blue;
+            }
+
+            .markdown a:hover {
+            opacity: 0.6;
+            }
+
+            .markdown h3 {
+            margin: 0;
+            padding: 0;
+            text-transform: uppercase;
+            }
+        `}</style>
     </Layout>
 );
 
